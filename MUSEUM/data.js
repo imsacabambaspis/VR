6a29f2f805da9af1a58df86e2605c47ef00ff288
imsacabambaspis <sakanadegozaimasu@gmail.com>
2024-04-03 14:41:15 +0800

    0403V3

---
+++ MUSEUM/data.js
@@ -0,0 +1,247 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-jpg",
+      "name": "首頁jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.187247096732115,
+        "pitch": -0.03415402176787374,
+        "fov": 1.1397100333332975
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.776882453913128,
+          "pitch": -0.20027177403570207,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0795797211842508,
+          "pitch": -0.37042428249869985,
+          "title": "特展廣告",
+          "text": "街景拍攝時是芭比展"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "內部",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.38113417050664467,
+        "pitch": 0.0101931189341542,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7867107761592145,
+          "pitch": 0.05108255824536201,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 2.473948916530505,
+          "pitch": 0.5141365914948324,
+          "rotation": 12.566370614359176,
+          "target": "0-jpg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "大廳",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.0962283340475718,
+        "pitch": 0.08396061325923654,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8062152793452313,
+          "pitch": 0.03361671123256471,
+          "rotation": 4.71238898038469,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.10509465290888009,
+          "pitch": 0.2951342769112557,
+          "rotation": 3.9269908169872414,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.7823748381380966,
+          "pitch": -0.009173516023702533,
+          "rotation": 7.0685834705770345,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2973517004728201,
+          "pitch": -0.12358581544529912,
+          "title": "特展區",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "左邊",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.8147471881964652,
+        "pitch": 0.2273959989840293,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8139570168938466,
+          "pitch": 0.292484457197709,
+          "rotation": 3.9269908169872414,
+          "target": "0-jpg"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5658547039238684,
+          "pitch": -0.10420697462285311,
+          "title": "其他展場",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "樓梯間",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.0219536205829307,
+        "pitch": -0.019433088603719284,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9945911787916177,
+          "pitch": 0.5461304422270477,
+          "rotation": 3.141592653589793,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2642701925748554,
+          "pitch": -0.3626393938615653,
+          "title": "彩繪玻璃",
+          "text": "獨具特色的彩繪玻璃"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
